c60c867adb087b23db8e59c07a5300f13604fa70
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-04-16 13:49:56 +0600

    react new project

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { Suspense } from 'react'
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import Navbar from './Components/Navbar/Navbar'
+import PricingOption from './Components/PricingOption/PricingOption'
+import ResultsChart from './Components/DaisyPricing/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './Components/MarksChart/MarksChart'
+
+const pricingPromise=fetch('pricingData.json')
+.then(res=>res.json())
+
+const marksPromise=axios.get('marksData.json')
+
+function App() {
+
+
+  return (
+    <>
+
+
+
+      <header>
+        <Navbar></Navbar>
+      {/* <DaisyNav></DaisyNav> */}
+      </header>
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOption pricingPromise={pricingPromise} >
+
+          </PricingOption>
+        </Suspense>
+
+        <Suspense>
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
+
+        <ResultsChart></ResultsChart>
+
+      </main>
+
+    </>
+  )
+}
+
+export default App
